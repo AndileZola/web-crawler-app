0663e6e3366c26aca8e9335b028e20c78e839264
Andile Zola <aczola@gmail.com>
2023-11-07 09:49:08 +0200

    Add files via upload
    init

---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        //Log all crawled urls
+        //string logFilePath  =  $"C:\\Crawler_Logs\\crawl_logging_{DateTime.Now.ToString("ddmmyyyyhhmm")}.txt";
+        //if (!Directory.Exists(logFilePath))
+        //    Directory.CreateDirectory(logFilePath);
+        string logFilePath = $"crawl_logging_{DateTime.Now.ToString("ddmmyyyyhhmm")}.txt";
+        TextWriterTraceListener textListener = new TextWriterTraceListener(logFilePath);
+        Trace.Listeners.Add(textListener);
+        Trace.AutoFlush = true;
+        Console.WriteLine("Start Time:{0}", DateTime.Now);
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        Console.WriteLine("Please enter startUrl: ");
+        string startUrl = Console.ReadLine(); // Read user input
+        Console.WriteLine($"StartUrl: {startUrl}");
+        //string startUrl = "https://mhlontlolm.gov.za/"; //"https://en.wikipedia.org/wiki/South_Africa"; // Replace with your starting URL
+        var crawler = new WebCrawler(startUrl);
+
+        //Start Crawling. This might take a while
+        await crawler.CrawlAsync();
+        sw.Stop();
+        Console.WriteLine("Elapsed={0}", sw.Elapsed);
+        Console.WriteLine("Stop Time:{0}", DateTime.Now);
+        Trace.Close();
+        textListener.Close();
+        Console.WriteLine("Crawling Complete!!!!!");
+        Console.WriteLine("Log file: " + logFilePath);
+        Console.ReadKey();
+    }
+}
+class WebCrawler
+{
+    private HttpClient httpClient;
+    private string baseUrl;
+    private ConcurrentQueue<string> urlsToCrawl;
+    private HashSet<string> crawledUrls;
+
+    public WebCrawler(string startUrl)
+    {
+        httpClient = new HttpClient();
+        baseUrl = new Uri(startUrl).Host;
+        urlsToCrawl = new ConcurrentQueue<string>();
+        crawledUrls = new HashSet<string>();
+
+        urlsToCrawl.Enqueue(startUrl);
+    }
+
+    public async Task CrawlAsync()
+    {
+        //Avoid crawling the same url twice.
+        while (urlsToCrawl.TryDequeue(out string url)) 
+        {
+            //Url already crawed. Don't crawl again
+            if (crawledUrls.Contains(url))
+                continue; // URL already crawled
+
+            Console.WriteLine("Crawled: {0}", url);
+            Trace.WriteLine("Crawled: " + url);
+
+            //Get the html code fo the page into a string
+            var pageContent = await DownloadPageAsync(url);
+            if (pageContent == null)
+                continue; // Error while downloading
+
+            //Get all links found in that page
+            var links = ExtractLinksInHtmlPage(url, pageContent);
+
+            foreach (var link in links)
+            {
+                //All all links to the list of URL's yet to crawl
+                urlsToCrawl.Enqueue(link);
+            }
+
+            //collection of only links found in thd downloaded page
+            crawledUrls.Add(url);
+        }
+    }
+
+    private async Task<string> DownloadPageAsync(string url)
+    {
+        try
+        {
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occurred while downloading the page: {ex.Message}");
+            return null;
+        }
+    }
+
+    public static List<string> ExtractLinksInHtmlPage(string url, string htmlContent)
+    {
+        var extractedLinks = new List<string>();
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(htmlContent);
+
+        var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
+
+        if (links != null && links.Count > 0)
+        {
+            foreach (var link in links)
+            {
+                Uri absoluteUri;
+                var href = link.GetAttributeValue("href", "");
+                if (Uri.TryCreate(new Uri(url), href, out absoluteUri) && absoluteUri.Host == new Uri(url).Host)
+                {
+                    extractedLinks.Add(absoluteUri.AbsoluteUri);
+                }
+            }
+        }
+
+        return extractedLinks;
+    }
+
+    private async Task CrawlPageAsync(string url, List<string> results)
+    {
+        try
+        {
+            if (crawledUrls.Contains(url))
+            {
+                return; // this URL is already crawled
+            }
+
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var htmlContent = await response.Content.ReadAsStringAsync();
+
+            var document = new HtmlDocument();
+            document.LoadHtml(htmlContent);
+
+            var links = document.DocumentNode.SelectNodes("//a[@href]");
+            if (links != null)
+            {
+                foreach (var link in links)
+                {
+                    var href = link.GetAttributeValue("href", "");
+                    if (!string.IsNullOrWhiteSpace(href))
+                    {
+                        Uri absoluteUri;
+                        if (Uri.TryCreate(new Uri(url), href, out absoluteUri) && absoluteUri.Host == baseUrl)
+                        {
+                            urlsToCrawl.Enqueue(absoluteUri.AbsoluteUri);
+                            results.Add(absoluteUri.AbsoluteUri);
+                        }
+                    }
+                }
+            }
+
+            crawledUrls.Add(url);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occurred while crawling the page: {ex.Message}");
+        }
+    }
+}
+
---
+++ Program2.cs
@@ -0,0 +1,267 @@
+﻿////using HtmlAgilityPack;
+//using HtmlAgilityPack;
+//using System;
+//using System.Collections.Concurrent;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Threading;
+//using System.Xml;
+
+//class WebCrawler
+//{   
+//    private string baseUrl;
+//    //private string startUrl;
+//    private HttpClient httpClient;
+//    private List<string> urlsToCrawl; //List<string> extractedUrls = new List<string>();
+
+//    private readonly List<string> crawledUrls;
+//    private readonly object urlsToCrawlLock = new object(); // Lock object for synchronization
+//    int crawledCount = 0;
+
+//    //public WebCrawler(string startUrl)
+//    //{
+//    //    this.startUrl = startUrl;
+//    //    this.baseUrl = new Uri(startUrl).Host;
+//    //    this.httpClient = new HttpClient();
+//    //    this.urlsToCrawl = new List<string>();
+//    //    this.crawledUrls = new List<string>();
+//    //}
+
+//    public WebCrawler()
+//    {
+//        this.httpClient = new HttpClient();
+//        this.urlsToCrawl = new List<string>();
+//        this.crawledUrls = new List<string>();
+//    }
+
+//    public List<string> Crawl(string startUrl)
+//    {
+//        this.baseUrl = new Uri(startUrl).Host;
+//        var tasks = new List<Task>();
+//        var results = new List<string>();
+//        AddToUrlsToCrawl(startUrl);    
+
+//        while (true)
+//        {
+//            string urlToCrawl = GetNextUrlToCrawl();
+//            if (urlToCrawl == null)
+//                break; // No more URLs to crawl
+
+//            if (!crawledUrls.Contains(urlToCrawl))
+//            {
+//                crawledUrls.Add(urlToCrawl);
+//                tasks.Add(Task.Factory.StartNew(() =>
+//                {
+//                    CrawlPage(urlToCrawl, results);
+//                }));
+//            }
+//        }
+
+//        Task.WhenAll(tasks).Wait();
+//        return results;
+//    }
+
+//    public static List<string> ExtractLinksInHtmlPage(string url, string htmlContent)
+//    {
+//        var extractedLinks = new List<string>();
+//        var htmlDocument = new HtmlDocument(); //Using HtmlAgilityPack: https://html-agility-pack.net/traversing
+//        htmlDocument.LoadHtml(htmlContent);
+
+//        var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
+
+//        if (links != null && links.Count > 0)
+//        {          
+//            foreach (var link in links)
+//            {
+//                Uri absoluteUri;
+//                var href = link.GetAttributeValue("href", "");
+//                bool isValidUrl = Uri.TryCreate(new Uri(url), href, out absoluteUri);
+//                if (isValidUrl)
+//                {
+//                    extractedLinks.Add(absoluteUri.AbsoluteUri);
+//                }
+//            }
+//        }
+
+//        return extractedLinks;
+//    }
+
+
+//    private void AddToUrlsToCrawl(string url)
+//    {
+//        lock (urlsToCrawlLock)
+//        {
+//            urlsToCrawl.Add(url);
+//        }
+//    }
+
+//    private string GetNextUrlToCrawl()
+//    {
+//        lock (urlsToCrawlLock)
+//        {
+//            if (urlsToCrawl.Count > 0)
+//            {
+//                string url = urlsToCrawl[0];
+//                urlsToCrawl.RemoveAt(0); //Do not crawl the same url twice
+//                return url;
+//            }
+//            return null;
+//        }
+//    }
+
+//    public void ParseHtml(string htmlContent)
+//    {
+//        var xmlDocument = new XmlDocument();
+//        xmlDocument.LoadXml(htmlContent);
+
+//        //ParseNodes(xmlDocument.ChildNodes);
+//        if (xmlDocument.ChildNodes != null && xmlDocument.ChildNodes.Count > 0)
+//        {
+//            foreach (XmlNode node in xmlDocument.ChildNodes)
+//            {
+//                if (node is XmlElement element)
+//                {
+//                    if (element.Name == "a" && element.Attributes["href"] != null)
+//                    {
+//                        string href = element.Attributes["href"].Value;
+//                        urlsToCrawl.Add(href);
+//                    }
+//                    // Recursively process child nodes
+//                    ParseNodes(element.ChildNodes);
+//                }
+//            }
+//        }
+//    }
+
+//    private static void ParseNodes(XmlNodeList nodes)
+//    {
+//        foreach (XmlNode node in nodes)
+//        {
+//            if (node is XmlElement element)
+//            {
+//                if (element.Name == "a" && element.Attributes["href"] != null)
+//                {
+//                    string href = element.Attributes["href"].Value;
+//                    //urlsToCrawl.Add(href);
+//                }              
+//                // Recursively process child nodes
+//                ParseNodes(element.ChildNodes);
+//            }
+//        }
+//    }
+
+//    private void CrawlPage2(string url, List<string> results)
+//    {
+//        try
+//        {
+//            var response = httpClient.GetAsync(url).Result;
+//            var html = response.Content.ReadAsStringAsync().Result;
+
+//            //Using HtmlAgilityPack library:  https://html-agility-pack.net/?z=codeplex
+//            //var htmlDocument = new HtmlDocument();
+//            //htmlDocument.LoadHtml(html); //Get serialized html page
+
+//            //var allLinksInPage = htmlDocument.DocumentNode.SelectNodes("//a[@href]"); //Get all links in the serilized html page
+
+
+//            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+//            {
+//                string htmlContent = client.DownloadString(url);
+//                var extractedLinks = ExtractLinksInHtmlPage(url,htmlContent);
+//            }
+
+//            //ExtractLinksInHtmlPage(html);
+
+
+//            //if (urlsToCrawl != null && urlsToCrawl.Count > 0)
+//            //{
+//            //    foreach (var link in urlsToCrawl)
+//            //    {
+//            //        var href = link.GetAttributeValue("href", "");
+//            //        if (!string.IsNullOrWhiteSpace(href))
+//            //        {
+//            //            Uri uri = new Uri(url);
+//            //            if (Uri.TryCreate(new Uri(url), href, out uri) && uri.Host == baseUrl)
+//            //            {
+//            //                urlsToCrawl.Add(uri.AbsoluteUri);
+//            //                results.Add(uri.AbsoluteUri);
+//            //            }
+//            //        }
+//            //    }
+//            //}
+//        }
+//        catch (Exception ex)
+//        {
+//            throw new Exception($"Error occured while crawling the page: {ex.Message}");
+//        }
+//    }
+
+//    private List<string> CrawlPage(string url, List<string> results)
+//    {
+//        try
+//        {
+//            //var response = httpClient.GetAsync(url).Result;
+//            //var html = response.Content.ReadAsStringAsync().Result;
+
+//            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+//            {
+//                string htmlContent = client.DownloadString(url);
+//                //var extractedLinks = ExtractLinksInHtmlPage(url, htmlContent);
+
+//                var document = new HtmlDocument();
+//                document.LoadHtml(htmlContent);
+
+//                var links = document.DocumentNode.SelectNodes("//a[@href]").Skip(1);
+//                if (links != null)
+//                {
+//                    foreach (var link in links)
+//                    {
+//                        var href = link.GetAttributeValue("href", "");
+//                        if (!string.IsNullOrWhiteSpace(href))
+//                        {
+//                            //if (crawledUrls.Contains(url))
+//                            //    break;
+
+//                            Uri absoluteUri;
+//                            if (Uri.TryCreate(new Uri(url), href, out absoluteUri) && absoluteUri.Host == baseUrl) //only links that are under the same hostname as startUrl.
+//                            {
+//                                //urlsToCrawl.Add(absoluteUri.AbsoluteUri);
+//                                //results.Add(absoluteUri.AbsoluteUri);
+//                                crawledUrls.Add(absoluteUri.AbsoluteUri);
+//                                //var all = CrawlPage(absoluteUri.AbsoluteUri, results);
+//                                Crawl(absoluteUri.AbsoluteUri);
+//                                //results.AddRange(all);
+//                                //Crawl only once
+//                                urlsToCrawl.Remove(absoluteUri.AbsoluteUri);
+//                                crawledCount = results.Count();
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            return results;
+//        }
+//        catch (Exception ex)
+//        {
+//            throw new Exception($"Error occured while crawling the page: {ex.Message}");
+//        }
+//        return null;
+//    }
+//}
+
+//class Program2
+//{
+//    static void Main(string[] args)
+//    {
+//        string startUrl = "https://en.wikipedia.org/wiki/South_Africa"; // Replace with your starting URL
+//        var crawler = new WebCrawler();
+//        var crawledUrls = crawler.Crawl(startUrl);
+
+//        foreach (var url in crawledUrls)
+//        {
+//            Console.WriteLine(url);
+//        }
+//    }
+//}
